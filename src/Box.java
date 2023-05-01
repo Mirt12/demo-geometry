e0df85a856f0307b16e64e7d86a3b428e901d24d
Mirt12 <Mirt12@yandex.ru>
2023-05-01 19:23:19 +0200

    refactor box class add constructor

--- src/Box.java
+++ src/Box.java
@@ -2,15 +2,14 @@ public class Box {
     double width;
     double height;
     double depth;
-
+    Box() {
+        System.out.println("Constructor of the Object run");
+        width = 10;
+        height = 10;
+        depth = 10;
+    }
+    //method for the volume:
     double volume() {
         return width * height * depth;
-    }
-
-    //to set sizes of the parallerogram:
-    void setDim(double w, double h, double d){
-        width = w;
-        height = h;
-        depth = d;
     }
 }
