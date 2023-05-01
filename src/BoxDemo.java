a2490a8bdd9592064509afd6f2240ebf0a6becf5
Mirt12 <Mirt12@yandex.ru>
2023-05-01 17:15:24 +0200

    refactor boxdemo class use volume method

--- src/BoxDemo.java
+++ src/BoxDemo.java
@@ -2,7 +2,7 @@ public class BoxDemo {
     public static void main(String args[]) {
         Box mybox1 = new Box();
         Box mybox2 = new Box();
-        double vol;
+        //to set values for both objects:
         mybox1.width = 10;
         mybox1.height = 20;
         mybox1.depth = 15;
@@ -10,9 +10,7 @@ public class BoxDemo {
         mybox2.height = 6;
         mybox2.depth = 9;
         //Volume of parallelepiped:"
-        vol = mybox1.width * mybox1.height * mybox1.depth;
-        System.out.println("Volume is: " + vol);
-        vol = mybox2.width * mybox2.height * mybox2.depth;
-        System.out.println("Volume is: " + vol);
+        mybox1.volume();
+        mybox2.volume();
     }
 }
