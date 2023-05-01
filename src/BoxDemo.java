97d884039481ac23c6ee3de1e6191b4799512848
Mirt12 <Mirt12@yandex.ru>
2023-05-01 20:03:58 +0200

    refactor boxdemo class use constructor

--- src/BoxDemo.java
+++ src/BoxDemo.java
@@ -3,9 +3,6 @@ public class BoxDemo {
         Box mybox1 = new Box();
         Box mybox2 = new Box();
         double vol;
-        //to set values for both objects:
-        mybox1.setDim(10, 20, 15);
-        mybox2.setDim(3, 6, 9);
         //Volume of parallelepiped:"
         vol = mybox1.volume();
         System.out.println("Volume is: "+ vol);
